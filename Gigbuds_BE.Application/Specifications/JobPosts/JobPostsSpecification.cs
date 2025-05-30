0368bd14846e0c87aba3edc86ab561aef99b969e
quanggiap <nguyenquanggiap642004@gmail.com>
2025-05-30 20:03:59 +0700

    #28 remove age filter

--- Gigbuds_BE.Application/Specifications/JobPosts/JobPostsSpecification.cs
+++ Gigbuds_BE.Application/Specifications/JobPosts/JobPostsSpecification.cs
@@ -35,8 +35,6 @@ namespace Gigbuds_BE.Application.Specifications.JobPosts
         (!jobPostSearchParams.SalaryFrom.HasValue || x.Salary >= jobPostSearchParams.SalaryFrom.Value) &&
         (!jobPostSearchParams.SalaryTo.HasValue || x.Salary <= jobPostSearchParams.SalaryTo.Value) &&
         (!jobPostSearchParams.JobPositionId.HasValue || x.JobPositionId == jobPostSearchParams.JobPositionId.Value) &&
-        (!jobPostSearchParams.AgeFrom.HasValue || x.AgeRequirement >= jobPostSearchParams.AgeFrom.Value) &&
-        (!jobPostSearchParams.AgeTo.HasValue || x.AgeRequirement <= jobPostSearchParams.AgeTo.Value) &&
         (!jobPostSearchParams.JobTimeFrom.HasValue || x.CreatedAt >= jobPostSearchParams.JobTimeFrom.Value) &&
         (!jobPostSearchParams.JobTimeTo.HasValue || x.ExpireTime <= jobPostSearchParams.JobTimeTo.Value) &&
         (!jobPostSearchParams.IsMale.HasValue || x.IsMale == jobPostSearchParams.IsMale.Value) &&
