f83541b84b868066b8c7596598580ae293d780ac
quanggiap <nguyenquanggiap642004@gmail.com>
2025-06-18 01:42:35 +0700

    #50 Finish change status for job applicants

---
+++ Gigbuds_BE.Application/Features/JobApplications/Commands/UpdateJobApplicationStatusCommandHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using Gigbuds_BE.Application.Interfaces.Repositories;
+using Gigbuds_BE.Application.Specifications.JobApplications;
+using Gigbuds_BE.Domain.Entities.Jobs;
+using MediatR;
+
+namespace Gigbuds_BE.Application.Features.JobApplications.Commands;
+
+public class UpdateJobApplicationStatusCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<UpdateJobApplicationStatusCommand, bool>
+{
+    public async Task<bool> Handle(UpdateJobApplicationStatusCommand request, CancellationToken cancellationToken)
+    {
+        var spec = new GetJobSpecificationById(request.JobApplicationId);
+        var jobApplication = await unitOfWork.Repository<JobApplication>().GetBySpecificationAsync(spec);
+        if (jobApplication == null)
+        {
+            return false;
+        }
+        jobApplication.ApplicationStatus = request.Status;
+        jobApplication.UpdatedAt = DateTime.UtcNow;
+        unitOfWork.Repository<JobApplication>().Update(jobApplication);
+        await unitOfWork.CompleteAsync();
+        return true;
+    }
+}
